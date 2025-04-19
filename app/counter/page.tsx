314f259467f45cd351a06718514b7b5bd5488282
Muhammad Haris <114726020+MuhammadHarisBaloch@users.noreply.github.com>
2025-04-19 17:21:26 +0500

    [FEAT] Added Icons

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -1,10 +1,22 @@
-import { Button } from "@mantine/core";
+import { Flex } from "@mantine/core";
 import Link from "next/link";
-
+import {
+  IconSettingsFilled,
+  IconReload,
+  IconExclamationCircleFilled,
+  IconArrowLeft,
+} from "@tabler/icons-react";
 export default function Counter() {
   return (
-    <Button component={Link} href="/">
-      Back
-    </Button>
+    <Flex px="lg" pt="lg">
+      <Link href="/">
+        <IconArrowLeft color="black" size={30} />
+      </Link>
+      <Flex gap="lg" justify="center" w="100vw">
+        <IconExclamationCircleFilled size={30} />
+        <IconSettingsFilled size={30} />
+        <IconReload size={30} />
+      </Flex>
+    </Flex>
   );
 }
