8ee9d89ddf9830cb14640f9ce49eac96e4dd38ea
sagar18iOSdeveloper <sagar18.iosdeveloper@gmail.com>
2025-04-26 09:34:25 +0100

    [FEAT] basic timer design added 🚀

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -1,3 +1,39 @@
-export default function timer() {
-  return <>Timer Page</>;
+"use client";
+import { Box, Button, Flex, Stack, Text, Title } from "@mantine/core";
+import { IconArrowLeft } from "@tabler/icons-react";
+import Link from "next/link";
+import RoundedButton from "../../components/Counter/RoundedButton";
+
+export default function TimerPage() {
+  return (
+    <Stack h="100vh">
+      <Flex align="center" justify="space-between">
+        <Box component={Link} href="/" m="lg">
+          <IconArrowLeft className="hover-expand" color="black" size={30} />
+        </Box>
+        <Text fz="1.5rem">Click on timer to edit!</Text>
+        <Box />
+      </Flex>
+
+      <Flex align="center" justify="space-between" p="lg">
+        <Button
+          className="hover-expand pointer"
+          variant="outline"
+          color="black"
+          fz="lg"
+          size="lg"
+          radius="xl"
+        >
+          Start
+        </Button>
+        <Title order={3} fz="28rem" fw={300}>
+          0
+        </Title>
+        <Stack>
+          <RoundedButton text="+" />
+          <RoundedButton text="-" />
+        </Stack>
+      </Flex>
+    </Stack>
+  );
 }
